df41531c585130c94d51df747a7b21e6286edee1
Victor Yamchinov <kodiers@gmail.com>
2022-03-17 01:09:17 +0300

    Initial commit

---
+++ src/test/java/com/tfl/photoappapigateway/PhotoAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfl.photoappapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
